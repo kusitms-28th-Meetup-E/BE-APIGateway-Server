59d6a023eb8bb5d931dae1d9979ee1a72be505b7
seungyeonnnnnni <jeongsy8928@soongsil.ac.kr>
2023-11-01 04:18:52 +0900

    feat : global error handling(not found jwt, invalid jwt)

--- src/main/java/gwangjang/server/handler/ModuleConfig.java
+++ src/main/java/gwangjang/server/handler/ModuleConfig.java
@@ -2,7 +2,6 @@ package gwangjang.server.handler;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,7 +12,6 @@ public class ModuleConfig {
     public ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
-        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         return objectMapper;
     }
 }
